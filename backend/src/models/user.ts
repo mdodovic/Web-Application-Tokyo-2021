c2b1d776cb77d3390a05c8092716ce9d99d378fa
mdodovic <matija.dodovic@gmail.com>
2021-09-10 00:33:26 +0200

    added user model

---
+++ backend/src/models/user.ts
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+let User = new Schema(
+    {
+        username: {
+            type: String
+        },
+        password: {
+            type: String
+        },
+        firstname: {
+            type: String
+        },
+        lastname: {
+            type: String
+        },
+        nationality: {
+            type: String
+        },
+        email: {
+            type: String
+        },
+        type: {
+            type: String
+        },
+        accepted: {
+            type: Number
+        },
+    }
+);
+
+export default mongoose.model("User", User, "users");
+
+
+
