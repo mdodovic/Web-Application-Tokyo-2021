95649b9dc6af7aef98bfe70e0262cf0a791341b2
mdodovic <matija.dodovic@gmail.com>
2021-09-16 15:23:14 +0200

    create backend discipline model

---
+++ backend/src/models/discipline.ts
@@ -0,0 +1,32 @@
+/*
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+let Discipline = new Schema(
+    {
+        disciplineName: {
+            type: String
+        },
+        single: {
+            type: Boolean
+        },
+        minPlayers: {
+            type: Number
+        },
+        maxPlayers: {
+            type: Number
+        },
+        accepted: {
+            type: Boolean
+        }
+
+    }
+);
+
+export default mongoose.model("Discipline", Discipline, "?");
+
+*/
+
+
+
