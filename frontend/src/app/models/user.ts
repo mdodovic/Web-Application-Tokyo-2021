fc127ed5be25ddbd93fd24c3a94e17280883f3f3
mdodovic <matija.dodovic@gmail.com>
2021-09-16 15:30:21 +0200

    create frontend user model

---
+++ frontend/src/app/models/user.ts
@@ -0,0 +1,10 @@
+export class User {
+    username: string;
+    password: string;
+    firstname: string;
+    lastname: string;
+    nationality: string;
+    email: string;
+    type: string;
+    accepted: number;
+}
