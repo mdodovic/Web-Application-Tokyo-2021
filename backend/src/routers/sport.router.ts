b6c9729c5e55619ede2cd44127221578f61ef8db
mdodovic <matija.dodovic@gmail.com>
2021-09-21 12:00:22 +0200

    fixed accepted sport fetching

---
+++ backend/src/routers/sport.router.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import { SportController } from '../controllers/sport.controller';
+
+const sportRouter = express.Router();
+
+sportRouter.route('/fetchAllSports').get(
+    (req, res) => new SportController().fetchAllSports(req, res)
+);
+
+sportRouter.route('/fetchAllSportsForConfirmation').get(
+    (req, res) => new SportController().fetchAllSportsForConfirmation(req, res)
+);
+
+
+
+sportRouter.route('/acceptSport').post(
+    (req, res) => new SportController().acceptSport(req, res)
+);
+
+
+
+export default sportRouter;
+
