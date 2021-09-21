5bb06f43c82349d4544a0ae3b45d1972afa6ce6d
mdodovic <matija.dodovic@gmail.com>
2021-09-21 11:56:42 +0200

    updated competition service

---
+++ frontend/src/app/competition.service.ts
@@ -0,0 +1,77 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CompetitionService {
+
+  uri = 'http://localhost:4000'
+
+  constructor(private http: HttpClient) { }
+
+  fetchAllCompetitionsService() {
+    return this.http.get(`${this.uri}/competitions/fetchAllCompetitions`);
+  }
+
+  fetchByDelegateService(delegateUsername) {
+    console.log(delegateUsername)
+    const data = {
+      delegateUsername: delegateUsername
+    }
+
+    return this.http.post(`${this.uri}/competitions/fetchByDelegate`, data);
+  }
+
+  addCompetitionService(sportName, disciplineName, initiallySingle, actAsSingle, gender,
+    possibleLocations, begin, end, delegate,
+    groupNumber, competitorsNumber,
+    roundsNumber,
+    resultType, resultFormat, rankPlayers, comment,
+    participants) {
+
+    const data = {
+      sportName: sportName,
+      disciplineName: disciplineName,
+      initiallySingle: initiallySingle,
+      actAsSingle: actAsSingle,
+      gender: gender,
+
+      possibleLocations: possibleLocations,
+      begin: begin,
+      end: end,
+      delegate: delegate,
+
+      groupNumber: groupNumber,
+      competitorsNumber: competitorsNumber,
+
+      roundsNumber: roundsNumber,
+
+      resultType: resultType,
+      resultFormat: resultFormat,
+      rankPlayers: rankPlayers,
+      comment: comment,
+
+      participants: participants,
+      finished: false
+    }
+
+    return this.http.post(`${this.uri}/competitions/addCompetition`, data);
+  }
+
+  finishCompetitionService(sportName, disciplineName, gender) {
+    const data = {
+      sportName: sportName,
+      disciplineName: disciplineName,
+      gender: gender
+    }
+
+    return this.http.post(`${this.uri}/competitions/finishCompetition`, data);
+  }
+
+  fetchAllFinishedCompetitionsService() {
+    return this.http.get(`${this.uri}/competitions/fetchAllFinishedCompetitions`);
+  }
+
+
+}
