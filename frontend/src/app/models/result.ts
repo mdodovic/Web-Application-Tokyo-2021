02963e623aff9a015b5288d597f423c627354143
mdodovic <matija.dodovic@gmail.com>
2021-09-21 11:53:09 +0200

    added result model

---
+++ frontend/src/app/models/result.ts
@@ -0,0 +1,15 @@
+import { FinalCollection } from "./finalCollection";
+import { GroupCollection } from "./groupCollection";
+import { TreeCollection } from "./treeCollection";
+
+export class Result {
+    sportName: string;
+    disciplineName: string;
+    gender: string;
+
+    final: Array<FinalCollection>;
+    tree: Array<TreeCollection>;
+    group: Array<GroupCollection>;
+    timetabledEndCompetition: boolean;
+    groupPhaseFinished: boolean;
+}
