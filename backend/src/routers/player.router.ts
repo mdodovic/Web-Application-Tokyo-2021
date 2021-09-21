da11df69fd816bc5ae9c375a8f090b324841a6d3
mdodovic <matija.dodovic@gmail.com>
2021-09-21 11:59:56 +0200

    update player router

---
+++ backend/src/routers/player.router.ts
@@ -0,0 +1,45 @@
+import express from 'express';
+import { PlayerController } from '../controllers/player.controller';
+
+const playerRouter = express.Router();
+
+playerRouter.route('/fetchByFirstnameAndLastname').post(
+    (req, res) => new PlayerController().fetchByFirstnameAndLastname(req, res)
+);
+
+playerRouter.route('/addPlayer').post(
+    (req, res) => new PlayerController().addPlayer(req, res)
+);
+
+playerRouter.route('/addDisciplineToPlayer').post(
+    (req, res) => new PlayerController().addDisciplineToPlayer(req, res)
+);
+
+playerRouter.route('/fetchByNationalitySportDisciplineAndGender').post(
+    (req, res) => new PlayerController().fetchByNationalitySportDisciplineAndGender(req, res)
+);
+
+playerRouter.route('/removePlayer').post(
+    (req, res) => new PlayerController().removePlayer(req, res)
+);
+
+playerRouter.route('/removeDisciplineFromPlayer').post(
+    (req, res) => new PlayerController().removeDisciplineFromPlayer(req, res)
+);
+
+playerRouter.route('/fetchByNationality').post(
+    (req, res) => new PlayerController().fetchByNationality(req, res)
+);
+
+playerRouter.route('/addMedal').post(
+    (req, res) => new PlayerController().addMedal(req, res)
+);
+
+
+playerRouter.route('/fetchAllPlayers').get(
+    (req, res) => new PlayerController().fetchAllPlayers(req, res)
+);
+
+
+
+export default playerRouter;
